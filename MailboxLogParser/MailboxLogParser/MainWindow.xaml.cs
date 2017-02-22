d6a22214ff520c5ee678ceecffd2cc455b4895bc
Edwin Huber <edwin-huber@users.noreply.github.com>
2017-02-22 14:45:37 +0100

    Basic functionality for parsing and viewing mailbox log entries Still need to properly hook up all UI pieces

--- MailboxLogParser/MailboxLogParser/MainWindow.xaml.cs
+++ MailboxLogParser/MailboxLogParser/MainWindow.xaml.cs
@@ -13,6 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.ComponentModel;
+using System.Diagnostics;
+
+using MailboxLogParser.Common.Reporting;
+using MailboxLogParser.Common.Parsing;
+using MailboxLogParser.Common.Reporting.BasicReport;
+using MailboxLogParser.Common.Reporting.Reports;
+using System.Data;
+using Microsoft.Win32;
+
 namespace MailboxLogParser
 {
     /// <summary>
@@ -20,9 +30,208 @@ namespace MailboxLogParser
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
+        private List<int> HiddenGridRowIndexes = new List<int>();
+        private static StringBuilder ResponseDataBuilder = new StringBuilder();
+        private ViewModel viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            viewModel = new ViewModel();
+            dgMain.ItemsSource = viewModel.View;
+        }
+
+        #region Private Methods
+
+        public static string ConvertToBase16(string base64)
+        {
+            string base16 = string.Empty;
+
+            byte[] bytes = Convert.FromBase64String(base64);
+            return GetStringFromBytes(bytes);
+        }
+
+        public static string GetStringFromBytes(byte[] bytes)
+        {
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                ret.Append(Convert.ToString(b, 16).PadLeft(2, '0'));
+            }
+
+            return ret.ToString();
+        }
+
+        
+
+        private void UpdateStatus(string statusMessage)
+        {
+            Debug.WriteLine("UpdateStatus called.");
+            this.StatusLabel.Content = statusMessage;
+
+            int visibleRows = this.dgMain.Items.Count;
+            // ToDo:
+            // int hiddenRows = this.LogReportGrid.Rows.Count - visibleRows;
+
+            this.VisibleRowsLabel.Content = "Visible Rows: " + visibleRows;
+            // this.HiddenRowsLabel.Content = "Hidden Rows: " + hiddenRows;
+            this.UpdateLayout();
+        }
+
+        private void ClearLogs()
+        {
+            viewModel.Clear();
+
+            this.dgMain.ItemsSource = null; //  LogReportGrid.Rows.Clear();
+
+            this.LogDetailHeaders.Text = string.Empty;
+            this.LogDetailRequest.Text = string.Empty;
+            this.txtSearch.Text = string.Empty;
+        }
+
+        private void LoadLogsToGrid(string[] mailboxLogFiles)
+        {
+            Stopwatch total = new Stopwatch();
+            total.Start();
+
+            try
+            {
+                Stopwatch logs = new Stopwatch();
+                logs.Start();
+
+                try
+                {
+                    // Load the mailbox logs into the report
+                    viewModel.LoadMailboxLogs(mailboxLogFiles);
+                }
+                finally
+                {
+                    logs.Stop();
+                    Debug.WriteLine("LoadLogsToGrid: Report.LoadMailboxLogs took " + logs.ElapsedMilliseconds + " milliseconds.");
+                }
+
+                
+            }
+            finally
+            {
+                total.Stop();
+                Debug.WriteLine("LoadReportToGrid: Overall took " + total.ElapsedMilliseconds + " milliseconds.");
+            }
+        }
+
+        /// <summary>
+        /// ToDo: This logic should eventually just pull the entries from the data grid into the separate
+        /// view windows
+        /// </summary>
+        /// <param name="rowId"></param>
+        private void LoadReportRowData(string rowId)
+        {
+            Stopwatch total = new Stopwatch();
+            total.Start();
+
+            try
+            {
+                var data = from r in viewModel.Report.ReportRows
+                           where (string)r.Columns["Name"] == rowId
+                           select r.RawData as string;
+
+                if (data.Count() > 1)
+                {
+                    throw new ApplicationException("More than one match in ReportRows for RowId, '" + rowId + "'");
+                }
+
+                if (data.Count() == 0)
+                {
+                    this.LogDetailHeaders.Text = null;
+                    this.LogDetailResponse.Text = null;
+                    this.LogDetailRequest.Text = null;
+                    return;
+                }
+
+
+                ResponseDataBuilder.Clear();
+                ResponseDataBuilder.Append(data.First());
+
+                if (ResponseDataBuilder.ToString().IndexOf("RequestBody") > 0)
+                {
+                    this.LogDetailHeaders.Text = ResponseDataBuilder.ToString().Remove(ResponseDataBuilder.ToString().IndexOf("RequestBody")); // data.First(); // need to filter out header only
+                    // start at RequestHeader : or the very top, stop at RequestBody : 
+                }
+
+                if (ResponseDataBuilder.ToString().IndexOf("RequestBody") > 0)
+                {
+
+                    int intReqBod = ResponseDataBuilder.ToString().LastIndexOf("RequestBody");
+                    if (intReqBod == -1)
+                        intReqBod = 0;
+
+                    int intRespBod = ResponseDataBuilder.ToString().LastIndexOf("ResponseHeader");
+                    if (intRespBod == -1)
+                    { intRespBod = intReqBod; };
+                    this.LogDetailRequest.Text = ResponseDataBuilder.ToString().Substring(intReqBod, intRespBod - intReqBod); // need to filter out request // then filter response
+                }
+                else
+                {
+                    this.LogDetailRequest.Text = data.First();
+                }
+
+
+                if (ResponseDataBuilder.ToString().IndexOf("ResponseBody") > 0)
+                {
+                    this.LogDetailResponse.Text = ResponseDataBuilder.ToString().Substring(ResponseDataBuilder.ToString().LastIndexOf("ResponseHeader")); // from ResponseHeader : to end.
+                }
+            }
+            finally
+            {
+                total.Stop();
+                Debug.WriteLine("LoadReportRowData: Overall took " + total.ElapsedMilliseconds + " milliseconds.");
+            }
+        }
+
+        #endregion
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // ToDo: Set wait cursor?
+
+                var dialog = new OpenFileDialog();
+                dialog.Title = "Pick mailbox log files to parse...";
+                dialog.CheckFileExists = true;
+                dialog.Multiselect = true;
+
+                if (dialog.ShowDialog() == true)
+                {
+                    try
+                    {
+                        // ToDo: Set wait cursor?
+                        UpdateStatus("Loading...");
+                        LoadLogsToGrid(dialog.FileNames);
+                        dgMain.ItemsSource = viewModel.View;
+                    }
+                    finally
+                    {
+                        // ToDo: Reset wait cursor?
+                        
+                    }
+                }
+
+                UpdateStatus("Mailbox logs imported.");
+            }
+            finally
+            {
+                // ToDo: Reset wait cursor?
+            }
+        }
+
+        private void dgMain_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = this.dgMain.SelectedItems[0] as DataRowView;
+
+            LoadReportRowData(item["Id"].ToString());
         }
     }
 }
---
+++ MailboxLogParser/MailboxLogParser/ViewModel.cs
@@ -0,0 +1,71 @@
+﻿using MailboxLogParser.Common.Reporting;
+using MailboxLogParser.Common.Parsing;
+using MailboxLogParser.Common.Reporting.BasicReport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MailboxLogParser
+{
+    class ViewModel
+    {
+        public ReportBase Report = new BasicReport();
+        private DataTable table;
+        public DataView View;
+
+        /// <summary>
+        /// Not thread safe, as we are updating the DataTable
+        /// </summary>
+        /// <param name="mailboxLogFiles"></param>
+        public void LoadMailboxLogs(string[] mailboxLogFiles)
+        {
+            this.Report.LoadMailboxLogs(mailboxLogFiles);
+            int index = 0;
+            table = new DataTable();
+            setColumns(Report.ReportColumns);
+            
+            foreach (ReportRowBase reportRow in this.Report.ReportRows)
+            {
+                DataRow dr = table.NewRow();
+                dr["Id"] = index;
+                index++;
+
+                setRow(dr, reportRow);
+
+                table.Rows.Add(dr);
+            }
+
+            View = table.DefaultView;
+        }
+
+        private void setRow(DataRow dr, ReportRowBase reportRow)
+        {
+            foreach (string columnName in reportRow.Columns.Keys)
+            {
+                dr[columnName] = (reportRow[columnName] == null) ? DBNull.Value : reportRow[columnName] ;
+            }
+        }
+
+        private void setColumns(List<ReportColumnBase> cols)
+        {
+            DataColumn dc;
+            dc = new DataColumn("Id", Type.GetType("System.Int32")); // Col 0
+            table.Columns.Add(dc);
+            foreach (ReportColumnBase col in cols)
+            {
+                dc = new DataColumn(col.ColumnName, col.ColumnValueType);
+                table.Columns.Add(dc);
+            }
+        }
+
+        internal void Clear()
+        {
+            this.Report = new BasicReport();
+            View = null;
+            table = null;
+        }
+    }
+}
